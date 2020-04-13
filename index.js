de6fd688a0ecf6a055cce8c9e6c6e520f1dde0fa
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-04-14 00:15:30 +0900

    Add: router function in entryfile

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const router = require('./routes/index');
 const port = 5000;
 
 const app = express();
+
+app.use(bodyParser());
+app.use(cors());
+app.use('/', router);
 
 try {
   app.listen(port, () => console.log('Sevrer status ...OK'))
