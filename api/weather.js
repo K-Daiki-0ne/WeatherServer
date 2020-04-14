425cee82f956748de090e5c34402b624a0373188
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-04-15 00:05:11 +0900

    Add: router function

--- api/weather.js
+++ api/weather.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
 require('dotenv').config()
 
-export const getWeather = async (lat, lon) => {
+exports.getWeather = async (lat, lon) => {
   const baseUrl = process.env.API_URL;
   const apiKey = process.env.API_KEY;
-  const reqUrl = `${baseUrl}/${apiKey}`;
+  const reqUrl = `${baseUrl}/${apiKey}/${lat},${lon}`;
   try {
     const data = await axios.get(reqUrl);
     return data;
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const { getWeather } = require('../api/weather');
 const router = express.Router();
 
-router.get('/:lat/:lon', async(req, res) => {
-  const { location } = req.params;
-  const result = await getWeather(location.lat, location.lon);
-  res.send(result);
+router.get('/:lat/:lon', async (req, res) => {
+  const latitude = req.params.lat;
+  const longitude = req.params.lon;
+  const result = await getWeather(latitude, longitude);
+  const data = await result.data;
+  res.send(data);
 })
 
 module.exports = router
