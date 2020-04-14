936fdd8ddd4555b91168f5bc1c1cb76f9e9a5ad1
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-04-14 23:52:36 +0900

    Add: api url & key

--- api/weather.js
+++ api/weather.js
@@ -2,13 +2,13 @@ const axios = require('axios');
 require('dotenv').config()
 
 export const getWeather = async (lat, lon) => {
-  const baseUrl
-  const apiKey
-  const reqUrl
+  const baseUrl = process.env.API_URL;
+  const apiKey = process.env.API_KEY;
+  const reqUrl = `${baseUrl}/${apiKey}`;
   try {
-    console.log('Get API ...OK');
     const data = await axios.get(reqUrl);
     return data;
+    console.log('Get API ...OK');
   } catch (error) {
     console.error('Get API ...NO');
   }
